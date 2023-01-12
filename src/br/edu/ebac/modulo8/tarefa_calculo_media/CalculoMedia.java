31bb6771093ce20cf23a5c313b165ec2f830b3bf
ricardo <rosastaricardo84@gmail.com>
2023-01-12 20:34:13 -0300

    exercicio modulo 8

---
+++ src/br/edu/ebac/modulo8/tarefa_calculo_media/CalculoMedia.java
@@ -0,0 +1,18 @@
+package br.edu.ebac.modulo8.tarefa_calculo_media;
+
+public class CalculoMedia {
+
+    public static void main(String[] args) {
+
+        double nota1 = 8.7;
+        double nota2 = 9.5;
+        double nota3 = 7.0;
+
+        double media;
+
+        media = nota1 + nota2 + nota3 / 3;
+
+        System.out.println("A média das notas é: "+ media);
+
+    }
+}
